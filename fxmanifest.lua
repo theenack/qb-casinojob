64c10fcff7b001c2e2d724c5c600be7cc113b048
theenack <90981544+theenack@users.noreply.github.com>
2023-01-04 20:34:04 -0500

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'QB-Casino Job'
+version '1.0.0'
+author 'enack'
+
+shared_scripts {
+    'config/sh_config.lua'
+}
+
+client_scripts {
+	'client/*.lua'
+}
+
+server_scripts {
+    'server/*.lua'
+}
+
+dependencies {
+    'qb-menu'
+}
